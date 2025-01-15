f09936fcc621be98da264bb6b47a9d9931956b3f
Mladen <ciricm@uwindsor.ca>
2025-01-15 15:28:25 -0500

    whatrever

--- ci/scripts/utils.sh
+++ ci/scripts/utils.sh
@@ -29,29 +29,28 @@ check_git_repository() {
 
 set_bionemo_home() {
     set +u
-    # make your changes here
     if [ -z "$BIONEMO_HOME" ]; then
         echo "\$BIONEMO_HOME is unset. Setting \$BIONEMO_HOME to repository root."
-
-        # Ensure repository is clean
-        if ! check_git_repository; then
-            echo "Failed to set \$BIONEMO_HOME due to repository state." >&2
-            return 1
-        fi
-
-        REPOSITORY_ROOT=$(git rev-parse --show-toplevel 2>/dev/null)
-        if [ $? -ne 0 ]; then
-            echo "ERROR: Could not determine the repository root. Ensure you're in a Git repository." >&2
-            return 1
-        fi
-
-        BIONEMO_HOME="${REPOSITORY_ROOT}"
-        echo "Setting \$BIONEMO_HOME to: $BIONEMO_HOME"
+	export BIONEMO_HOME=/workspace/bionemo2
+#        # Ensure repository is clean
+#        if ! check_git_repository; then
+#            echo "Failed to set \$BIONEMO_HOME due to repository state." >&2
+#            return 1
+#        fi
+#
+#        REPOSITORY_ROOT=$(git rev-parse --show-toplevel 2>/dev/null)
+#        if [ $? -ne 0 ]; then
+#            echo "ERROR: Could not determine the repository root. Ensure you're in a Git repository." >&2
+#            return 1
+#        fi
+#
+#        BIONEMO_HOME="${REPOSITORY_ROOT}"
+#        echo "Setting \$BIONEMO_HOME to: $BIONEMO_HOME"
     fi
     set -u
 
     # Change directory to BIONEMO_HOME or exit if failed
-    cd "${BIONEMO_HOME}" || { echo "ERROR: Could not change directory to \$BIONEMO_HOME: $BIONEMO_HOME" >&2; return 1; }
+#    cd "${BIONEMO_HOME}" || { echo "ERROR: Could not change directory to \$BIONEMO_HOME: $BIONEMO_HOME" >&2; return 1; }
 }
 
 
